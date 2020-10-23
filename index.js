55cb188946087805e80fedbdec49f91ebbe7efa7
NjorogeVictor <njorogevictor4@gmail.com>
2020-10-23 14:06:37 +0300

    Refactor

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-exports.myDate = function() {
-  const date = new Date();
-  return date;
+// exports.myDate = function() {
+//   const date = new Date();
+//   return date;
+// };
+
+const generateDate = function() {
+  const currdate = new Date();
+  return currdate;
 };
+
+module.exports = generateDate;
